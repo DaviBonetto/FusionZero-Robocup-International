629cfcb026b67b2674525bed1e8471d272fb8f28
BlanketOmelette <aidanzcc@gmail.com>
2024-12-06 22:05:03 +1300

    Changed U-Turn detection to increase reliability (instead of seeing left or right on U-Turns). Added stopping motors during Line-following functionality using cv2.waitKey for inputs.

---
+++ code/Line_Following/8_stop_start_modular_better_green/config.py
@@ -0,0 +1,17 @@
+from picamera2 import Picamera2
+from libcamera import Transform
+
+# Config variables
+WIDTH, HEIGHT = 320, 240
+FLIP = False
+X11 = False
+
+# Initialize Picamera2
+camera = Picamera2()
+
+# Create a correct transform object from libcamera.Transform
+camera_config = camera.create_preview_configuration(main={"format": "RGB888", "size": (WIDTH, HEIGHT)}, transform=Transform(vflip=FLIP, hflip=FLIP))
+
+# Configure and start the camera
+camera.configure(camera_config)
+camera.start()
