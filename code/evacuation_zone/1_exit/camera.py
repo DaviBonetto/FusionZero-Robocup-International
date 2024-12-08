11d9560c421dc7a6dfc8085590d45980e918dd20
Frederick Sun <fredericksun12@gmail.com>
2024-12-08 21:22:46 +1300

    added basic functionality

---
+++ code/evacuation_zone/1_exit/camera.py
@@ -0,0 +1,14 @@
+from picamera2 import Picamera2
+from libcamera import Transform
+
+from config import *
+
+# Initialize Picamera2
+camera = Picamera2()
+
+# Create a correct transform object from libcamera.Transform
+camera_config = camera.create_preview_configuration(main={"format": "RGB888", "size": (WIDTH, HEIGHT)}, transform=Transform(vflip=FLIP, hflip=FLIP))
+
+# Configure and start the camera
+camera.configure(camera_config)
+camera.start()
