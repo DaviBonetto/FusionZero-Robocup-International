545bdf8c3719e51e2640312f59a112b549fc9839
BlanketOmelette <aidanzcc@gmail.com>
2024-12-14 18:08:55 +1300

    Added LED test code

---
+++ code/Tests/led_on_off.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(13)
+
+while True:
+    led.on()
+    print("LED is ON")
+    sleep(2)
+    led.off()
+    print("LED is OFF")
+    sleep(2)
