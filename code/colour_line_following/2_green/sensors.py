f0d2e4b972be13074d044d565f1d220fedd0afec
Frederick Sun <fredericksun12@gmail.com>
2025-01-08 23:50:38 +1300

    Created simple green line following code. However, double green detection needs more testing.

---
+++ code/colour_line_following/2_green/sensors.py
@@ -0,0 +1,120 @@
+import time
+import os
+import board
+import adafruit_ads7830.ads7830 as ADC
+from adafruit_ads7830.analog_in import AnalogIn
+
+import motors
+
+i2c = board.I2C()
+adc = ADC.ADS7830(i2c)
+
+CALIBRATION_FILE = "/home/fusion/FusionZero-Robocup-International/code/colour_line_following/calibration_values.txt"
+
+def load_calibration_values():
+    """
+    Load calibration values from environment variables.
+    
+    return: Loads min calibrated values and max calibrated values from os.
+
+    """
+    if os.path.exists(CALIBRATION_FILE):
+        with open(CALIBRATION_FILE, "r") as f:
+            lines = f.readlines()
+            min_values = list(map(int, lines[0].strip().split(",")))
+            max_values = list(map(int, lines[1].strip().split(",")))
+
+            print("Loaded calibrated min and max values:")
+            print(f"Min: {min_values}")
+            print(f"Max: {max_values}")
+    else:
+        print("Couldn't find saved calibrated values. Reseting colour values to default.")
+        min_values = [255] * 7
+        max_values = [0] * 7
+
+    return min_values, max_values
+
+calibrated_min, calibrated_max = load_calibration_values()
+
+def read_analog(display=False):
+    """
+    Reads first 7 values from the ads7830 (colour sensor values).
+
+    return: Array, size 7, raw adc readings from 0 to 255, with an order (0 to 7): Outer Left, Middle Left, Middle, Middle Right, Outer Right, Back Left, Back Right.
+    """
+
+    analog_readings = []
+    for channel in range(7):
+        analog_readings.append(int(AnalogIn(adc, channel).value / 256))
+    
+    if display:
+        print(f"Raw C: {analog_readings}, ", end="")
+
+    return analog_readings
+
+def read_mapped_analog(display=False):
+    """
+    Reads raw analog data then maps values from 0 to 100 based on their min and max values
+
+    return: mapped values as an array, size 7.
+    """
+    global calibrated_min, calibrated_max
+
+    raw_analog_values = read_analog()
+
+    mapped_values = []
+    for i in range(7):
+        mapped_value = (raw_analog_values[i] - calibrated_min[i]) * 100 / (calibrated_max[i] - calibrated_min[i])
+        mapped_values.append(int(mapped_value))
+    
+    if display:
+        print(f"C: {mapped_values}, ", end="")
+        read_analog(True)
+        print()
+
+    return mapped_values
+
+def update_calibration(min_values, max_values):
+    for i in range(1000):
+        analog_values = read_analog()
+
+        for i in range(7):
+            min_values[i] = min(min_values[i], analog_values[i])
+            max_values[i] = max(max_values[i], analog_values[i])
+        
+        time.sleep(0.001)
+    
+    return min_values, max_values
+
+def save_calibration_values(min_values, max_values):
+    """
+    Save calibration values to environment variables.
+    """
+    os.environ["CALIBRATED_MIN"] = ",".join(map(str, min_values))
+    os.environ["CALIBRATED_MAX"] = ",".join(map(str, max_values))
+    print("Calibration values saved.")
+
+def calibration(auto_calibrate):
+    """
+    Resets calibrated colour sensor values that are used for mapped values which is needed for line following.
+
+    param auto_calibrate: If True it turns the motors automatically to callibrate without the need for a person to move the car, if False someone will move the car over the line to calibrate it.
+    """
+    
+    calibrated_min = [255] * 7
+    calibrated_max = [0] * 7
+
+    if auto_calibrate:
+        motors.run(30, -30)
+    
+    calibrated_min, calibrated_max = update_calibration(calibrated_min, calibrated_max)
+
+    if auto_calibrate:
+        motors.run(-30, 30)
+
+    calibrated_min, calibrated_max = update_calibration(calibrated_min, calibrated_max)
+
+    print(f"Min Values: {calibrated_min}")
+    print(f"Max Values: {calibrated_max}")
+
+    save_calibration_values(calibrated_min, calibrated_max)
