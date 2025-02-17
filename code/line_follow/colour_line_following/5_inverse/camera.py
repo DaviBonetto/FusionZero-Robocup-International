09efe0fac0d4e81160eaedad6ba988568fb67a47
Frederick Sun <fredericksun12@gmail.com>
2025-02-17 19:17:39 +1300

    redirecting folder structure

---
+++ code/line_follow/colour_line_following/5_inverse/camera.py
@@ -0,0 +1,39 @@
+import cv2
+from picamera2 import Picamera2
+from libcamera import Transform
+import oled_display
+import config
+    
+camera = None
+
+def initialise():
+    global camera
+    try:
+        camera = Picamera2()
+
+        camera_config = camera.create_preview_configuration(main={"format": "RGB888", "size": (config.WIDTH, config.HEIGHT)}, transform=Transform(vflip=config.FLIP, hflip=config.FLIP))
+        camera.configure(camera_config)
+        camera.start()
+        
+        print(["Camera", "✓"])
+        oled_display.text("Camera: ✓", 0, 40)
+    except Exception as e:
+        print(f"Camera failed to initialise: {e}")
+        oled_display.text("Camera: X", 0, 40)
+
+    if config.X11:
+        try:
+            cv2.startWindowThread()
+            print(["X11", "✓"])
+            oled_display.text("X11: ✓", 60, 40)
+        except Exception as e:
+            print(["X11", "X", f"{e}"])
+            oled_display.text("X11: X", 60, 40)
+
+def close():
+    global camera
+    camera.close()
+
+def capture_array():
+    global camera
+    return camera.capture_array()
---
+++ code/line_follow/sensor_fusion_line_following/1_wheg_testing/line.py
@@ -0,0 +1,9 @@
+import time
+import colour
+import laser_sensors
+import touch_sensors
+import motors
+import evacuation_zone
+
+def follow_line():
+    print(colour.read())
