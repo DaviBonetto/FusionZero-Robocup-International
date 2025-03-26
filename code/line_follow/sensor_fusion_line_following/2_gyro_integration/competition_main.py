46bf6abc0d8ed09a17bb3757d6db67b9a23de85f
Duality <67132160+D-uality@users.noreply.github.com>
2025-03-26 21:20:43 +1300

    inital idea, with a rocker switch program that runs on startup, allowing for starting and killing the competition_main.py which auto starts with line

---
+++ code/line_follow/sensor_fusion_line_following/2_gyro_integration/competition_main.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import time
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+modules_dir = os.path.abspath(os.path.join(current_dir, 'modules'))
+
+if modules_dir not in sys.path: sys.path.insert(0, modules_dir)
+
+import camera
+import laser_sensors
+import touch_sensors
+import oled_display
+import motors
+import gyroscope
+import led
+import line
+import config
+
+def main() -> None:
+    start_time = time.time()
+    oled_display.initialise()
+    laser_sensors.initialise()
+    touch_sensors.initialise()
+    motors.initialise()
+    camera.initialise("line")
+    gyroscope.initialise()
+    
+    motors.run(0, 0)
+    oled_display.reset()
+    led.off()
+    
+    config.update_log(["INITIALISATION", f"({time.time() - start_time:.2f})"], [24, 15])
+    print()
+    
+    try:
+        line.main()
+
+    finally:
+        camera.close()
+        oled_display.reset()
+        motors.run(0, 0)
+    
+if __name__ == "__main__": main()
