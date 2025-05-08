a971ac864b56301b24a735db28c54ee850a9c9b6
D-uality <fredericksun12@gmail.com>
2025-05-08 21:36:59 +1200

    updated with ability to record videos, formatted image naming, and also downscaling and original image scale

--- 5_ai_training_data/capture_script.py
+++ 5_ai_training_data/capture_script.py
@@ -1,6 +1,7 @@
-import cv2, sys, time
+import cv2, sys, time, datetime, os
 
 DEVICE_PATH = "/dev/video0"
+SAVE_DIR = "/home/frederick/FusionZero-Robocup-International/5_ai_training_data"
 
 def main() -> None:
     camera = cv2.VideoCapture(DEVICE_PATH, cv2.CAP_V4L2)
@@ -12,6 +13,8 @@ def main() -> None:
     camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'YUYV'))
     camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
+    os.makedirs("captures", exist_ok=True)
+
     try:
         while True:
             t0 = time.perf_counter()
@@ -20,8 +23,8 @@ def main() -> None:
             if not ok: continue
             
             # Display image
-            image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
             image = cv2.resize(image, (320, 240), interpolation=cv2.INTER_AREA)
+            image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)  
             cv2.imshow("image", image)
             
             # exit on q
@@ -30,6 +33,12 @@ def main() -> None:
             
             elif user_input == ord("c"):
                 print("Capture image")
+                
+                time_stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+                path = os.path.join(SAVE_DIR, f"{time_stamp}.jpg")
+
+                cv2.imwrite(path, image)
+                print(f"Captured {path}")
             
             # print capture rate
             print(f"{1 / (time.perf_counter() - t0):.2f}")
